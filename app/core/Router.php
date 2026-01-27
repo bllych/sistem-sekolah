cde4f2a3a1fa6828bef5a15ad8241bac4896eade
bllych <billychua56@gmail.com>
2026-01-27 14:29:07 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -8,7 +8,31 @@ class Router
     public function run()
     {
         // Routing logic goes here
-        echo 'Router is running';
+        $method =  $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'],  PHP_URL_PATH);
+        
+        if ($method == 'GET' && $uri == '/students') {
+            echo '<h1>Daftar Siswa</h1>';
+
+            echo '<p>Menampilkan daftar siswa.</p>';
+
+            return;
+
+
+
+        }
+        if ($method == 'GET' && $uri == '/students/create') {
+            echo '<h1>Tambah Siswa</h1>';
+
+            echo '<p>Menampilkan form tambah siswa.</p>';
+
+            return;
+
+
+
+        }
+        http_response_code(404);
+        echo '<h1>404 - Page Not Found</h1>';
     } 
 }
 
