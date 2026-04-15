c17297015c752b1b43f9a35a9e771b4b600b7b8d
bllych <billychua56@gmail.com>
2026-04-15 10:17:32 +0700

    feat: implement student detail page

--- app/views/students/show.php
+++ app/views/students/show.php
@@ -11,19 +11,19 @@
                 <div class="p-4 grid grid-cols-2 gap-4">
                     <div class="space-y-2">
                         <label class="font-bold block" for="name">Nama</label>
-                        <input class="px-4 py-2 border rounded-lg w-full" type="text" id="name" name="name" placeholder="Masukkan nama" readonly>
+                        <input value="<?= $student['name'] ?>" class="px-4 py-2 border rounded-lg w-full" type="text" id="name" name="name" placeholder="Masukkan nama" readonly>
                     </div>
                     <div class="space-y-2">
                         <label class="font-bold block" for="nis">NIS</label>
-                        <input class="px-4 py-2 border rounded-lg w-full" type="text" id="nis" name="nis" placeholder="Masukkan NIS" readonly>
+                        <input value="<?= $student['nis'] ?>" class="px-4 py-2 border rounded-lg w-full" type="text" id="nis" name="nis" placeholder="Masukkan NIS" readonly>
                     </div>
                     <div class="space-y-2">
                         <label class="font-bold block" for="class">Kelas</label>
-                        <input class="px-4 py-2 border rounded-lg w-full" type="text" id="class" name="class" placeholder="Masukkan kelas" readonly>
+                        <input value="<?= $student['class'] ?>" class="px-4 py-2 border rounded-lg w-full" type="text" id="class" name="class" placeholder="Masukkan kelas" readonly>
                     </div>
                     <div class="space-y-2">
                         <label class="font-bold block" for="phone-number">Nomor Telepon</label>
-                        <input class="px-4 py-2 border rounded-lg w-full" type="text" id="phone-number" name="phone-number" placeholder="Masukkan nomor telepon" readonly>
+                        <input value="<?= $student['phone_number'] ?>" class="px-4 py-2 border rounded-lg w-full" type="text" id="phone-number" name="phone-number" placeholder="Masukkan nomor telepon" readonly>
                     </div>
                     <div class="flex justify-end gap-4 col-span-2">
                         <a href="/students" class="px-4 py-2 rounded-lg bg-gray-400">Kembali</a>
